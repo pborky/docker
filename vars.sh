25484abd015143e0322be7e072501dd25276e272
Peter Boráros <pborky@pborky.sk>
2018-12-26 22:28:09 +0100

    Fix variables.

--- vars.sh
+++ vars.sh
@@ -6,8 +6,9 @@ CONT_NAME="octoprint"
 CONT_UID=666
 CONT_GID=666
 CONT_USER="octoprint"
+CONT_HOME="/var/octoprint"
 # groups of the new user, comma sepparated list
-CONT_GROUPS="dialout,$(id -gn)"
+CONT_GROUPS="dialout"
 # RUNTIME
 VOLUMES=$PWD/config:$CONT_HOME/.octoprint
 DEVICES=/dev/ttyACM0:/dev/ttyACM0
